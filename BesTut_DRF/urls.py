8360b6115f960bb319ca0745eef435cbc92a6c2f
silas003 <silaskumi4@gmail.com>
2024-01-26 01:20:14 +0000

    deloyed

--- BesTut_DRF/urls.py
+++ BesTut_DRF/urls.py
@@ -44,4 +44,6 @@ urlpatterns = [
 ]
 
 
-urlpatterns += [staticfiles_urlpatterns(),static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
