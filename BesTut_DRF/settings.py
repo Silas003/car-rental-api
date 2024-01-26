07a224560329068dd934b3265825e41c3b8cd961
silas003 <silaskumi4@gmail.com>
2024-01-26 01:11:16 +0000

    prod

--- BesTut_DRF/settings.py
+++ BesTut_DRF/settings.py
@@ -7,8 +7,7 @@ import dj_database_url
 
 
 env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
+
 )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
